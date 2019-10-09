ab59f9c717586fd02829e6bf994143482f017237
George Dadunashvili <g.dadunashvili@mailbox.org>
2019-10-09 16:16:27 +0200

    gitremoved stuff that was unspecific for the project

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-   name='difdfconvolver',
+   name='diffconvolver',
    version='0.1',
    description='Solve pde systems using FDM',
    author='George Dadunashvili',
