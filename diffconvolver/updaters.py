0c724efb1c810e49f326cc84f10eba55bd6242fd
George Dadunashvili <g.dadunashvili@mailbox.org>
2019-12-09 09:24:21 +0100

    added nonlinear updater, which is not working and changed eqns

--- diffconvolver/updaters.py
+++ diffconvolver/updaters.py
@@ -42,10 +42,13 @@ class Grid2D:
 
 
 def del_maker(ng, make_6th=False):
+    c1, i01 = sm.stencil_weights(1, -ng, ng)
     c2, i02 = sm.stencil_weights(2, -ng, ng)
     c4, i04 = sm.stencil_weights(4, -ng, ng)
     c6, i06 = sm.stencil_weights(6, -ng, ng)
 
+    dx = sm.stencil_matrix_pure(c1, i01, y_axes=0)
+    dy = sm.stencil_matrix_pure(c1, i01, y_axes=1)
     dxx = sm.stencil_matrix_pure(c2, i02, y_axes=0)
     dyy = sm.stencil_matrix_pure(c2, i02, y_axes=1)
 
@@ -66,17 +69,22 @@ def del_maker(ng, make_6th=False):
 
         return del2, del4, del6
     else:
-        return del2, del4
+        return dx, dy, dxx, dyy, del2, del4
 
 class Parameters:
-    def __init__(self, alpha: float = 0.1, gamma: float = 0.1, t_u: float = 1, t_psi: float = 1, sigma_eff=0.):
-        self.sigma_eff = sigma_eff
+    def __init__(self, alpha: float = 0.1, gamma: float = 0.1, t_u: float = 1, t_psi: float = 1,
+                 sigma: float=0., lmbda: float=0.1):
+        self.sigma = sigma
+        self.lmbda = lmbda
         self.alpha = alpha
         self.gamma = gamma
         self.t_u = t_u
         self.t_psi = t_psi
 
         self.ap1 = self.alpha + 1
+        self.sigma_eff = sigma
+        self.gamma_eff = self.gamma/(self.lmbda**2)
+        self.two_sigma_plus_1 = 2*self.sigma + 1
         self.tr = self.t_u/self.t_psi
 
 
@@ -177,9 +185,46 @@ def bulk_update_tube(u: np.ndarray, psi: np.ndarray,
     psi4 = sg.fftconvolve(psi, d4, mode='valid')
     # psi6 = sg.fftconvolve(psi, d6, mode='valid')
 
-    u[grid.bulk] -= dt*(u4 + (2-par.sigma_eff)*u2 + (1-par.sigma_eff)*u[grid.bulk] + psi2 + psi[grid.bulk])
-    psi[grid.bulk] += par.tr*dt*(u4 + u2 + par.ap1*psi2 - par.gamma*psi4)
+    # u[grid.bulk] -= dt*(u4 + (2-par.sigma_eff)*u2 + (1-par.sigma_eff)*u[grid.bulk] + psi2 + psi[grid.bulk])
+    u[grid.bulk] -= dt*(u4 + 2*u2 + u[grid.bulk] + psi2 + psi[grid.bulk])
+    psi[grid.bulk] += par.tr*dt*(u4 + u2 + par.two_sigma_plus_1 *psi2 - (par.gamma/par.lmbda**2)*psi4)
+    # psi[grid.bulk] += par.tr*dt*(u4 + u2 + par.ap1*psi2 - par.gamma*psi4)
 
+
+def nonlinear_bulk_update_tube(u: np.ndarray, psi: np.ndarray,
+                     dt: float,
+                     dx: np.ndarray, dy: np.ndarray, dxx: np.ndarray, dyy: np.ndarray,
+                     d2: np.ndarray, d4: np.ndarray, grid: Grid2D, par: Parameters):
+
+    ux = sg.fftconvolve(u, dx, mode='valid')
+    uy = sg.fftconvolve(u, dy, mode='valid')
+    uxx = sg.fftconvolve(u, dxx, mode='valid')
+
+    uyy = sg.fftconvolve(u, dyy, mode='valid')
+    u2 = sg.fftconvolve(u, d2, mode='valid')
+    # print(uxx.shape, u2.shape)
+    u4 = sg.fftconvolve(u, d4, mode='valid')
+    # u6 = sg.fftconvolve(u, d6, mode='valid')
+
+    psi2 = sg.fftconvolve(psi, d2, mode='valid')
+    psi4 = sg.fftconvolve(psi, d4, mode='valid')
+
+
+    psi_lin :np.ndarray = (u4 + u2 + par.ap1 * psi2)
+    # print(psi_lin.shape)
+    psi_underived = par.ap1 * psi[grid.bulk]*u[grid.bulk] + 0.5*(uxx - 3*uyy)*u[grid.bulk]
+    # print(psi_underived.shape)
+    psi_derived_x = sg.fftconvolve(psi_underived, dx, mode='same')
+    # print('psi_derived_x:',psi_derived_x.shape)
+    psi_derived_yy = sg.fftconvolve(psi_underived, dyy, mode='same')
+    psi_derived0 = sg.fftconvolve(psi_underived, d2, mode='same')
+
+    psi_derived = psi_derived0 + 2*(ux * psi_derived_x - u[grid.bulk] *psi_derived_yy)
+
+    u[grid.bulk] -= dt * (u4 + 2*u2 + u[grid.bulk] + psi[grid.bulk]
+                          + 1.5*u[grid.bulk]**2 + u[grid.bulk]*u2 +2*(u[grid.bulk] + uxx)*uyy + uy**2
+                          -0.5*(par.ap1)*psi[grid.bulk]**2 + (uyy-uxx)*psi[grid.bulk])
+    psi[grid.bulk] += par.tr * dt * (psi_lin + psi_derived)
 
 def bulk_update_u_averaged_tube(psi: np.ndarray,
                                 dt: float, d2: np.ndarray, d4: np.ndarray, d6: np.ndarray,
