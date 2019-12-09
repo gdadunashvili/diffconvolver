478082f92edf6e22f44f6c1d572ca895d3a9e6ec
George Dadunashvili <g.dadunashvili@mailbox.org>
2019-12-09 09:23:42 +0100

    added nonlinear updater

--- diffconvolver/__init__.py
+++ diffconvolver/__init__.py
@@ -1,2 +1,2 @@
-from diffconvolver.updaters import Grid2D, del_maker, Parameters, BcUpdate, bulk_update_flat, bulk_update_tube
+from diffconvolver.updaters import Grid2D, del_maker, Parameters, BcUpdate, bulk_update_flat, bulk_update_tube, nonlinear_bulk_update_tube
 from diffconvolver.stencil_maker import stencil_weights, stencil_matrix_pure, stencil_matrix_mixed
