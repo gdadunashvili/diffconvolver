474e7199933301a3efe15f2eefa6c7b34e07e56f
dadu <g.dadunashvili@mailbox.org>
2022-03-27 00:14:49 +0100

    fixed bug with Grid2D

--- diffconvolver/__init__.py
+++ diffconvolver/__init__.py
@@ -1,2 +1,3 @@
-from diffconvolver.updaters import Grid2D, del_maker, Parameters, BcUpdate, bulk_update_flat, bulk_update_tube, nonlinear_bulk_update_tube
+from diffconvolver.Grid2D import Grid2D
+from diffconvolver.updaters import del_maker, Parameters, BcUpdate, bulk_update_flat, bulk_update_tube, nonlinear_bulk_update_tube
 from diffconvolver.stencil_maker import stencil_weights, stencil_matrix_pure, stencil_matrix_mixed
